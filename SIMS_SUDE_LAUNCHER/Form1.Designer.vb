fc7a6ba3f589ad46072914772b17b0eb5c2aae8e
Trent Patterson <jtrent238@outlook.com>
2022-05-25 13:16:59 -0400

    Add files via upload

---
+++ SIMS_SUDE_LAUNCHER/Form1.Designer.vb
@@ -0,0 +1,313 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Launcher
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Launcher))
+        Me.GroupBox_Sims1 = New System.Windows.Forms.GroupBox()
+        Me.Label_Version_Sims1 = New System.Windows.Forms.Label()
+        Me.Button_Play_Sims1 = New System.Windows.Forms.Button()
+        Me.GroupBox_Sims2 = New System.Windows.Forms.GroupBox()
+        Me.Label_Version_Sims2 = New System.Windows.Forms.Label()
+        Me.Button_Play_Sims2 = New System.Windows.Forms.Button()
+        Me.GroupBox_Sims3 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox_Launcher_Sims3 = New System.Windows.Forms.CheckBox()
+        Me.Label_Version_Sims3 = New System.Windows.Forms.Label()
+        Me.Button_Play_Sims3 = New System.Windows.Forms.Button()
+        Me.GroupBox_Sims4 = New System.Windows.Forms.GroupBox()
+        Me.Label_Version_Sims4 = New System.Windows.Forms.Label()
+        Me.Button_DLC_Toggler_Sims4 = New System.Windows.Forms.Button()
+        Me.Button_Play_Sims4 = New System.Windows.Forms.Button()
+        Me.Label_createdby = New System.Windows.Forms.Label()
+        Me.Sims4_Logo = New System.Windows.Forms.PictureBox()
+        Me.Sims3_Logo = New System.Windows.Forms.PictureBox()
+        Me.Sims2_Logo = New System.Windows.Forms.PictureBox()
+        Me.Sims1_Logo = New System.Windows.Forms.PictureBox()
+        Me.Button_DLC_Toggler_Sims3 = New System.Windows.Forms.Button()
+        Me.GroupBox_Sims1.SuspendLayout()
+        Me.GroupBox_Sims2.SuspendLayout()
+        Me.GroupBox_Sims3.SuspendLayout()
+        Me.GroupBox_Sims4.SuspendLayout()
+        CType(Me.Sims4_Logo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Sims3_Logo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Sims2_Logo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Sims1_Logo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox_Sims1
+        '
+        Me.GroupBox_Sims1.Controls.Add(Me.Label_Version_Sims1)
+        Me.GroupBox_Sims1.Controls.Add(Me.Button_Play_Sims1)
+        Me.GroupBox_Sims1.Location = New System.Drawing.Point(12, 123)
+        Me.GroupBox_Sims1.Name = "GroupBox_Sims1"
+        Me.GroupBox_Sims1.Size = New System.Drawing.Size(200, 57)
+        Me.GroupBox_Sims1.TabIndex = 0
+        Me.GroupBox_Sims1.TabStop = False
+        Me.GroupBox_Sims1.Text = "The Sims"
+        '
+        'Label_Version_Sims1
+        '
+        Me.Label_Version_Sims1.AutoSize = True
+        Me.Label_Version_Sims1.Location = New System.Drawing.Point(6, 267)
+        Me.Label_Version_Sims1.Name = "Label_Version_Sims1"
+        Me.Label_Version_Sims1.Size = New System.Drawing.Size(55, 13)
+        Me.Label_Version_Sims1.TabIndex = 3
+        Me.Label_Version_Sims1.Text = "Version: X"
+        '
+        'Button_Play_Sims1
+        '
+        Me.Button_Play_Sims1.Location = New System.Drawing.Point(56, 19)
+        Me.Button_Play_Sims1.Name = "Button_Play_Sims1"
+        Me.Button_Play_Sims1.Size = New System.Drawing.Size(75, 23)
+        Me.Button_Play_Sims1.TabIndex = 2
+        Me.Button_Play_Sims1.Text = "Play"
+        Me.Button_Play_Sims1.UseVisualStyleBackColor = True
+        '
+        'GroupBox_Sims2
+        '
+        Me.GroupBox_Sims2.Controls.Add(Me.Label_Version_Sims2)
+        Me.GroupBox_Sims2.Controls.Add(Me.Button_Play_Sims2)
+        Me.GroupBox_Sims2.Location = New System.Drawing.Point(218, 123)
+        Me.GroupBox_Sims2.Name = "GroupBox_Sims2"
+        Me.GroupBox_Sims2.Size = New System.Drawing.Size(200, 57)
+        Me.GroupBox_Sims2.TabIndex = 0
+        Me.GroupBox_Sims2.TabStop = False
+        Me.GroupBox_Sims2.Text = "The Sims 2"
+        '
+        'Label_Version_Sims2
+        '
+        Me.Label_Version_Sims2.AutoSize = True
+        Me.Label_Version_Sims2.Location = New System.Drawing.Point(6, 267)
+        Me.Label_Version_Sims2.Name = "Label_Version_Sims2"
+        Me.Label_Version_Sims2.Size = New System.Drawing.Size(55, 13)
+        Me.Label_Version_Sims2.TabIndex = 3
+        Me.Label_Version_Sims2.Text = "Version: X"
+        '
+        'Button_Play_Sims2
+        '
+        Me.Button_Play_Sims2.Location = New System.Drawing.Point(63, 19)
+        Me.Button_Play_Sims2.Name = "Button_Play_Sims2"
+        Me.Button_Play_Sims2.Size = New System.Drawing.Size(75, 23)
+        Me.Button_Play_Sims2.TabIndex = 2
+        Me.Button_Play_Sims2.Text = "Play"
+        Me.Button_Play_Sims2.UseVisualStyleBackColor = True
+        '
+        'GroupBox_Sims3
+        '
+        Me.GroupBox_Sims3.Controls.Add(Me.CheckBox_Launcher_Sims3)
+        Me.GroupBox_Sims3.Controls.Add(Me.Label_Version_Sims3)
+        Me.GroupBox_Sims3.Controls.Add(Me.Button_Play_Sims3)
+        Me.GroupBox_Sims3.Location = New System.Drawing.Point(424, 123)
+        Me.GroupBox_Sims3.Name = "GroupBox_Sims3"
+        Me.GroupBox_Sims3.Size = New System.Drawing.Size(200, 57)
+        Me.GroupBox_Sims3.TabIndex = 0
+        Me.GroupBox_Sims3.TabStop = False
+        Me.GroupBox_Sims3.Text = "The Sims 3"
+        '
+        'CheckBox_Launcher_Sims3
+        '
+        Me.CheckBox_Launcher_Sims3.AutoSize = True
+        Me.CheckBox_Launcher_Sims3.Checked = True
+        Me.CheckBox_Launcher_Sims3.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox_Launcher_Sims3.Location = New System.Drawing.Point(90, 21)
+        Me.CheckBox_Launcher_Sims3.Name = "CheckBox_Launcher_Sims3"
+        Me.CheckBox_Launcher_Sims3.Size = New System.Drawing.Size(93, 17)
+        Me.CheckBox_Launcher_Sims3.TabIndex = 4
+        Me.CheckBox_Launcher_Sims3.Text = "Use Launcher"
+        Me.CheckBox_Launcher_Sims3.UseVisualStyleBackColor = True
+        '
+        'Label_Version_Sims3
+        '
+        Me.Label_Version_Sims3.AutoSize = True
+        Me.Label_Version_Sims3.Location = New System.Drawing.Point(6, 267)
+        Me.Label_Version_Sims3.Name = "Label_Version_Sims3"
+        Me.Label_Version_Sims3.Size = New System.Drawing.Size(55, 13)
+        Me.Label_Version_Sims3.TabIndex = 3
+        Me.Label_Version_Sims3.Text = "Version: X"
+        '
+        'Button_Play_Sims3
+        '
+        Me.Button_Play_Sims3.Location = New System.Drawing.Point(9, 17)
+        Me.Button_Play_Sims3.Name = "Button_Play_Sims3"
+        Me.Button_Play_Sims3.Size = New System.Drawing.Size(75, 23)
+        Me.Button_Play_Sims3.TabIndex = 2
+        Me.Button_Play_Sims3.Text = "Play"
+        Me.Button_Play_Sims3.UseVisualStyleBackColor = True
+        '
+        'GroupBox_Sims4
+        '
+        Me.GroupBox_Sims4.Controls.Add(Me.Label_Version_Sims4)
+        Me.GroupBox_Sims4.Controls.Add(Me.Button_Play_Sims4)
+        Me.GroupBox_Sims4.Location = New System.Drawing.Point(630, 123)
+        Me.GroupBox_Sims4.Name = "GroupBox_Sims4"
+        Me.GroupBox_Sims4.Size = New System.Drawing.Size(200, 57)
+        Me.GroupBox_Sims4.TabIndex = 0
+        Me.GroupBox_Sims4.TabStop = False
+        Me.GroupBox_Sims4.Text = "The Sims 4"
+        '
+        'Label_Version_Sims4
+        '
+        Me.Label_Version_Sims4.AutoSize = True
+        Me.Label_Version_Sims4.Location = New System.Drawing.Point(6, 267)
+        Me.Label_Version_Sims4.Name = "Label_Version_Sims4"
+        Me.Label_Version_Sims4.Size = New System.Drawing.Size(55, 13)
+        Me.Label_Version_Sims4.TabIndex = 3
+        Me.Label_Version_Sims4.Text = "Version: X"
+        '
+        'Button_DLC_Toggler_Sims4
+        '
+        Me.Button_DLC_Toggler_Sims4.Location = New System.Drawing.Point(691, 256)
+        Me.Button_DLC_Toggler_Sims4.Name = "Button_DLC_Toggler_Sims4"
+        Me.Button_DLC_Toggler_Sims4.Size = New System.Drawing.Size(75, 23)
+        Me.Button_DLC_Toggler_Sims4.TabIndex = 2
+        Me.Button_DLC_Toggler_Sims4.Text = "DLC Toggler"
+        Me.Button_DLC_Toggler_Sims4.UseVisualStyleBackColor = True
+        '
+        'Button_Play_Sims4
+        '
+        Me.Button_Play_Sims4.Location = New System.Drawing.Point(61, 19)
+        Me.Button_Play_Sims4.Name = "Button_Play_Sims4"
+        Me.Button_Play_Sims4.Size = New System.Drawing.Size(75, 23)
+        Me.Button_Play_Sims4.TabIndex = 2
+        Me.Button_Play_Sims4.Text = "Play"
+        Me.Button_Play_Sims4.UseVisualStyleBackColor = True
+        '
+        'Label_createdby
+        '
+        Me.Label_createdby.AutoSize = True
+        Me.Label_createdby.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Label_createdby.Location = New System.Drawing.Point(0, 180)
+        Me.Label_createdby.Name = "Label_createdby"
+        Me.Label_createdby.Size = New System.Drawing.Size(106, 13)
+        Me.Label_createdby.TabIndex = 2
+        Me.Label_createdby.Text = "Created By: jtrent238"
+        '
+        'Sims4_Logo
+        '
+        Me.Sims4_Logo.Image = Global.SIMS_SUDE_LAUNCHER.My.Resources.Resources.Sims4_Logo
+        Me.Sims4_Logo.Location = New System.Drawing.Point(630, 12)
+        Me.Sims4_Logo.Name = "Sims4_Logo"
+        Me.Sims4_Logo.Size = New System.Drawing.Size(200, 105)
+        Me.Sims4_Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Sims4_Logo.TabIndex = 1
+        Me.Sims4_Logo.TabStop = False
+        '
+        'Sims3_Logo
+        '
+        Me.Sims3_Logo.Image = Global.SIMS_SUDE_LAUNCHER.My.Resources.Resources.Sims3_Logo
+        Me.Sims3_Logo.Location = New System.Drawing.Point(424, 12)
+        Me.Sims3_Logo.Name = "Sims3_Logo"
+        Me.Sims3_Logo.Size = New System.Drawing.Size(200, 105)
+        Me.Sims3_Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Sims3_Logo.TabIndex = 1
+        Me.Sims3_Logo.TabStop = False
+        '
+        'Sims2_Logo
+        '
+        Me.Sims2_Logo.Image = Global.SIMS_SUDE_LAUNCHER.My.Resources.Resources.Sims2_Logo
+        Me.Sims2_Logo.Location = New System.Drawing.Point(218, 12)
+        Me.Sims2_Logo.Name = "Sims2_Logo"
+        Me.Sims2_Logo.Size = New System.Drawing.Size(200, 105)
+        Me.Sims2_Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Sims2_Logo.TabIndex = 1
+        Me.Sims2_Logo.TabStop = False
+        '
+        'Sims1_Logo
+        '
+        Me.Sims1_Logo.Image = Global.SIMS_SUDE_LAUNCHER.My.Resources.Resources.Sims1_Logo
+        Me.Sims1_Logo.Location = New System.Drawing.Point(12, 12)
+        Me.Sims1_Logo.Name = "Sims1_Logo"
+        Me.Sims1_Logo.Size = New System.Drawing.Size(200, 105)
+        Me.Sims1_Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Sims1_Logo.TabIndex = 1
+        Me.Sims1_Logo.TabStop = False
+        '
+        'Button_DLC_Toggler_Sims3
+        '
+        Me.Button_DLC_Toggler_Sims3.Location = New System.Drawing.Point(424, 256)
+        Me.Button_DLC_Toggler_Sims3.Name = "Button_DLC_Toggler_Sims3"
+        Me.Button_DLC_Toggler_Sims3.Size = New System.Drawing.Size(75, 23)
+        Me.Button_DLC_Toggler_Sims3.TabIndex = 2
+        Me.Button_DLC_Toggler_Sims3.Text = "DLC Toggler"
+        Me.Button_DLC_Toggler_Sims3.UseVisualStyleBackColor = True
+        '
+        'Launcher
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(846, 193)
+        Me.Controls.Add(Me.Label_createdby)
+        Me.Controls.Add(Me.Button_DLC_Toggler_Sims4)
+        Me.Controls.Add(Me.Button_DLC_Toggler_Sims3)
+        Me.Controls.Add(Me.Sims4_Logo)
+        Me.Controls.Add(Me.Sims3_Logo)
+        Me.Controls.Add(Me.Sims2_Logo)
+        Me.Controls.Add(Me.Sims1_Logo)
+        Me.Controls.Add(Me.GroupBox_Sims4)
+        Me.Controls.Add(Me.GroupBox_Sims3)
+        Me.Controls.Add(Me.GroupBox_Sims2)
+        Me.Controls.Add(Me.GroupBox_Sims1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Launcher"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "The Sims Super Ultimate Deluxe Edition"
+        Me.GroupBox_Sims1.ResumeLayout(False)
+        Me.GroupBox_Sims1.PerformLayout()
+        Me.GroupBox_Sims2.ResumeLayout(False)
+        Me.GroupBox_Sims2.PerformLayout()
+        Me.GroupBox_Sims3.ResumeLayout(False)
+        Me.GroupBox_Sims3.PerformLayout()
+        Me.GroupBox_Sims4.ResumeLayout(False)
+        Me.GroupBox_Sims4.PerformLayout()
+        CType(Me.Sims4_Logo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Sims3_Logo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Sims2_Logo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Sims1_Logo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox_Sims1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox_Sims2 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox_Sims3 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox_Sims4 As System.Windows.Forms.GroupBox
+    Friend WithEvents Sims1_Logo As System.Windows.Forms.PictureBox
+    Friend WithEvents Sims2_Logo As System.Windows.Forms.PictureBox
+    Friend WithEvents Sims3_Logo As System.Windows.Forms.PictureBox
+    Friend WithEvents Sims4_Logo As System.Windows.Forms.PictureBox
+    Friend WithEvents Label_Version_Sims1 As System.Windows.Forms.Label
+    Friend WithEvents Button_Play_Sims1 As System.Windows.Forms.Button
+    Friend WithEvents Label_Version_Sims2 As System.Windows.Forms.Label
+    Friend WithEvents Button_Play_Sims2 As System.Windows.Forms.Button
+    Friend WithEvents Label_Version_Sims3 As System.Windows.Forms.Label
+    Friend WithEvents Button_Play_Sims3 As System.Windows.Forms.Button
+    Friend WithEvents Label_Version_Sims4 As System.Windows.Forms.Label
+    Friend WithEvents Button_Play_Sims4 As System.Windows.Forms.Button
+    Friend WithEvents Button_DLC_Toggler_Sims4 As System.Windows.Forms.Button
+    Friend WithEvents Label_createdby As System.Windows.Forms.Label
+    Friend WithEvents CheckBox_Launcher_Sims3 As System.Windows.Forms.CheckBox
+    Friend WithEvents Button_DLC_Toggler_Sims3 As System.Windows.Forms.Button
+
+End Class
---
+++ SIMS_SUDE_LAUNCHER/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Launcher
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button_Play_Sims1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button_Play_Sims1.Click
+        Dim p As New Process()
+        p.StartInfo.FileName = ".\The Sims\sims.exe"
+        p.Start()
+    End Sub
+
+    Private Sub Button_Play_Sims2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button_Play_Sims2.Click
+        Dim p As New Process()
+        p.StartInfo.FileName = ".\Sims 2 Ultimate Collection\The Sims 2\TSBin\Sims2.exe"
+        p.Start()
+    End Sub
+
+    Private Sub Button_Play_Sims3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button_Play_Sims3.Click
+        If CheckBox_Launcher_Sims3.Checked Then
+            Dim p As New Process()
+            p.StartInfo.FileName = ".\The Sims 3\Game\Bin\Sims3Launcher.exe"
+            p.Start()
+        End If
+
+        If CheckBox_Launcher_Sims3.Checked = False Then
+            Dim p As New Process()
+            p.StartInfo.FileName = ".\The Sims 3\Game\Bin\TS3.exe"
+            p.Start()
+        End If
+    End Sub
+
+    Private Sub Button_Play_Sims4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button_Play_Sims4.Click
+        Dim p As New Process()
+        p.StartInfo.FileName = ".\The Sims 4\Game\Bin\TS4_x64.exe"
+        p.Start()
+    End Sub
+
+    Private Sub Button_DLC_Toggler_Sims3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button_DLC_Toggler_Sims3.Click
+        Dim p As New Process()
+        p.StartInfo.FileName = ".\The Sims 3\dlc-toggler.exe"
+        p.Start()
+    End Sub
+
+    Private Sub Button_DLC_Toggler_Sims4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button_DLC_Toggler_Sims4.Click
+        Dim p As New Process()
+        p.StartInfo.FileName = ".\The Sims 4\dlc-toggler.exe"
+        p.Start()
+    End Sub
+End Class
